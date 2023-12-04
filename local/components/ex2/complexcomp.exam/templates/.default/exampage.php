ec685e8841e01086217edd90d0b20f5aa1a87c7d
dev <dev@citrus-web.ru>
2023-12-04 13:42:46 +0300

    ex2-25 completed

---
+++ local/components/ex2/complexcomp.exam/templates/.default/exampage.php
@@ -0,0 +1,6 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<?
+//Вывести значения переменных
+?>
+<p>PARAM1 = <?=$arResult['VARIABLES']['PARAM1']?></p>
+<p>PARAM2 = <?=$arResult['VARIABLES']['PARAM2']?></p>
